ad197a52c9b6b4a18a7b71a493160bbe46b0cd79
Regina Pompeo <gina.celia.pompeo@gmail.com>
2024-04-12 10:20:02 -0300

    start: I did a WebScrapping in the metacritic games website.

---
+++ RM-97032-WEB-SCRAPPING/RM-97032-WEB-SCRAPPING/MetacriticParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RM_97032_WEB_SCRAPPING
+{
+    internal class MetacriticParser
+    {
+        public record CriticRating(string Title = "", int MetascoreAverage = -1, double UserScoreAverage = -1);
+    }
+}
---
+++ RM-97032-WEB-SCRAPPING/RM-97032-WEB-SCRAPPING/Program.cs
@@ -0,0 +1,51 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;                 
+
+class Program 
+{
+    static async Task Main(string[] args) 
+    {
+
+        var client = new HttpClient(); 
+        var url = "https://www.metacritic.com/game/five-nights-at-freddys/"; 
+
+        using (var response = await client.GetAsync(url)) 
+        {
+            if (response.IsSuccessStatusCode) 
+            {
+                var content = await response.Content.ReadAsStringAsync(); 
+                Console.WriteLine("HTML Content:"); 
+                Console.WriteLine(content); 
+
+                var doc = new HtmlDocument(); 
+                doc.LoadHtml(content); 
+
+                var titleNode = doc.DocumentNode.SelectSingleNode("/html/body/div[1]/div/div/div[2]/div[1]/div[1]/div/div/div[2]/div[3]/div[1]/div");
+                var title = titleNode?.InnerText?.Trim(); 
+
+                var metascoreNode = doc.DocumentNode.SelectSingleNode("/html/body/div[1]/div/div/div[2]/div[1]/div[1]/div/div/div[2]/div[3]/div[4]/div[1]/div/div[1]/div[2]/div/div/span");
+                var metascore = metascoreNode?.InnerText?.Trim();
+
+                var userscoreNode = doc.DocumentNode.SelectSingleNode("/html/body/div[1]/div/div/div[2]/div[1]/div[1]/div/div/div[2]/div[3]/div[4]/div[2]/div[1]/div[2]/div/div/span");
+                var userscore = userscoreNode?.InnerText?.Trim();
+
+                Console.WriteLine("Título do Jogo: " + (title ?? "Não achou o título do jogo.")); 
+                Console.WriteLine("Metascore: " + (metascore ?? "Não achou o metascore do jogo."));
+                Console.WriteLine("Userscore: " + (userscore ?? "Não achou o userscore do jogo."));
+            }
+            else 
+            {
+                Console.WriteLine("Erro no Http: " + response.StatusCode);
+            }
+        }
+
+        Console.WriteLine("Pressione alguma tecla para fechar o CMD");
+        Console.ReadKey();
+
+    }
+}
+
+
+
